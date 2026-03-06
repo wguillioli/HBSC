4a500a8be60e4f3c376179ccb5a64a9520e7c891
Walter Guillioli <wguillioli@gmail.com>
2026-03-05 16:44:20 -0800

    v1

--- eda.R
+++ eda.R
@@ -1,10 +1,12 @@
 
 require(tidyverse)
 
-dat <- "C:/Users/wagui/Downloads/Allrounds_CSV/HBSC2018OAed1.1.csv"
+dat <- "C:/MisLocalFiles/Github/HBSC/Allrounds_CSV/HBSC2018OAed1.1.csv"
 
 d <- read_delim(dat, delim = ";")
 
 glimpse(d)
 
 table(d$countryno)
+
+write_csv(d, "C:/MisLocalFiles/Github/HBSC/HBSC2018OAed1.1.csv")
